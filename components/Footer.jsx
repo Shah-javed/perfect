9e500e8ef40d277803bbb405a16738de50b8e915
Shah-javed <hafeedhasan1110@gmail.com>
2022-12-15 18:09:01 +0530

    second commit done

---
+++ components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+
+        <footer class="flex">
+            <div class="footer-col">
+                <img src="https://res.cloudinary.com/thirus/image/upload/v1628614672/logos/circleai_dm9slt.png" alt="Circle AI Logo" height="30" />
+                <p>The Company Tagline</p>
+            </div>
+            <div class="footer-col ml-auto">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h3>Contact Us</h3>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h3>Social</h3>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </footer>
+
+    )
+}
